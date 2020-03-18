a89b293e57b40c916130077b54491059f37a196f
yaoyu bao <yaobao5354@gmail.com>
2020-03-18 17:21:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
 
-  get 'multiply/:num1/:num2' do
+  get '/multiply/:num1/:num2' do
     @num1 = params[:num1]
 
     @num2 = params[:num2]
